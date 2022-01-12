a1958eeb065154d82a8666229903e2df544a9cb7
AndersonTorres <torres.anderson.85@protonmail.com>
2022-01-12 10:59:43 -0300

    [Patch from pkgsrc team] Don't use VLAs of non-POD types
    Source: http://cvsweb.netbsd.org/bsdweb.cgi/~checkout~/pkgsrc/math/aamath/patches/patch-expr.cc?rev=1.2&content-type=text/plain

--- expr.cc
+++ expr.cc
@@ -1192,7 +1192,7 @@ IntegralOnInterval::render_head() const
 
 class SumSymbol : public OpSymbol {
   public:
-	void render(Canvas& c, int r, int c) const;
+	void render(Canvas& canvas, int r, int c) const;
 };
 
 void
@@ -1208,7 +1208,7 @@ SumSymbol::render(Canvas& canvas, int r, int c) const
 
 class ProductSymbol : public OpSymbol {
   public:
-	void render(Canvas& c, int r, int c) const;
+	void render(Canvas& canvas, int r, int c) const;
 };
 
 void
@@ -1612,7 +1612,7 @@ Matrix::render() const
 	int cols = num_cols();
 	int rows = num_rows();
 
-	CanvasPtr ec[cols * rows];
+	CanvasPtr *ec = new CanvasPtr[cols * rows];
 	Size sz[cols * rows];
 
 	int row_height[rows];
@@ -1691,6 +1691,8 @@ Matrix::render() const
 	}
 
 	canvas->center();
+
+	delete[] ec;
 
 	return canvas;
 }
